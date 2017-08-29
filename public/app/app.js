a93727ae3606f5b98bda91da77d58450bcb72508
Ameya <deshpande.ameya@hotmail.com>
2017-08-29 09:27:13 -0400

    added course demo files

---
+++ public/app/app.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  // Define a module here
+  var app = angular.module('app', []);
+
+  app.provider('books', function (constants) {
+    this.$get = function () {
+            var appName = constants.APP_TITLE;
+            var appDesc = constants.APP_DESCRIPTION;
+
+            return{
+              appName:appName,
+              appDesc:appDesc
+            };
+    };
+    app.config(function ($provide, constants) {
+
+      console.log('title from the constants service : ' constants.APP_TITLE);
+
+    });
+  });
+
+  // This is the Option 1
+  // Services syntax using $provide.provider function
+  // $get has function
+  // app.config(function ($provide) {
+  //
+  //   $provide.provider('books', function(){
+  //     this.$get = function () {
+  //
+  //       var appName = 'Book logger';
+  //       var appDesc = 'Track which books you read.';
+  //
+  //       return{
+  //         appName:appName,
+  //         appDesc:appDesc
+  //       };
+  //     };
+  //   });
+  // });
+
+  //$provide.factory
+  //$provider.service - if needed to create a constructor used in inheritence
+
+})();
---
+++ public/app/books/BooksController.js
@@ -0,0 +1,15 @@
+(function () {
+
+  angular.module('app')
+  .controller('BooksController', BooksController);
+
+  function BooksController(books, dataService, logger) {
+    var vm = this;
+
+    vm.appName = books.appName;
+
+    vm.allBooks = dataService.getAllBooks();
+
+    logger.output('BooksController has been created. ');
+  }
+})();
